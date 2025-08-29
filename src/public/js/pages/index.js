5af63c0be6ef09e0f6e124f7907f678c2b034b5b
xexisss <adlusski@gmail.com>
2025-08-29 12:38:36 +0300

    feat(home page): add link to order.html

--- src/public/js/pages/index.js
+++ src/public/js/pages/index.js
@@ -2,3 +2,8 @@ const loan_calculator_button = document.getElementById('loan_calculator_button')
 loan_calculator_button.addEventListener('click', () => {
     window.location.href = '../loan_calculator.html';
 });
+
+const order_button = document.getElementById('order_button');
+order_button.addEventListener('click', () => {
+    window.location.href = '../order.html';
+});
