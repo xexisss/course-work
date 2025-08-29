b09484b187c0426a10f007c8aa61be1ba66ec903
xexisss <adlusski@gmail.com>
2025-08-29 12:30:48 +0300

    fix: change file structure

---
+++ src/public/js/pages/index.js
@@ -0,0 +1,4 @@
+const loan_calculator_button = document.getElementById('loan_calculator_button');
+loan_calculator_button.addEventListener('click', () => {
+    window.location.href = '../loan_calculator.html';
+});
