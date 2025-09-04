41fcf66cb364bbf3cd819fec81a21cd799f60f2f
xexisss <adlusski@gmail.com>
2025-09-04 21:47:25 +0300

    feat: add registration logic

---
+++ src/public/js/components/registration.js
@@ -0,0 +1,56 @@
+const form = document.getElementById('registerForm');
+const msg = document.getElementById('message');
+
+form.addEventListener('submit', async e => {
+    e.preventDefault();
+    msg.textContent = '';
+
+    const user = {
+        surname:    form.surname.value.trim(),
+        name:       form.name.value.trim(),
+        patronymic: form.patronymic.value.trim(),
+        birthdate:  form.birthdate.value.trim(),
+        phone:      form.phone.value.trim(),
+        email:      form.email.value.trim(),
+        nickname:   form.nickname.value.trim(),
+        password:   form.password.value,
+        passwordRepeat: form.password.value
+    };
+
+    if (!user.surname || !user.name || !user.birthdate || !user.phone || !user.email
+        || !user.nickname || !user.password) {
+        msg.textContent = 'Please fill in the required fields';
+        return;
+    }
+
+    if (user.password !== user.passwordRepeat) {
+        msg.textContent = 'The passwords do not match';
+        return;
+    }
+
+    try {
+        let res = await fetch(`http://localhost:3000/users?nickname=${user.nickname}`);
+        let exists = await res.json();
+        if(exists.length) {
+            msg.textContent = 'The nickname is already taken';
+            return
+        }
+        
+        delete user.passwordRepeat;
+        res = await fetch('http://localhost:3000/users', {
+            method:  'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body:    JSON.stringify(user)
+        });
+
+        if(!res.ok) throw new Error(res.statusText);
+
+        msg.style.color = 'green';
+        msg.textContent   = 'Регистрация прошла успешно!';
+        form.reset();
+    }   
+    catch (err) {
+        console.error(err);
+        msg.textContent = 'Registration error';
+    }
+});
