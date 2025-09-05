a39032ddd496415724e914ebf564c7b888ff3296
xexisss <adlusski@gmail.com>
2025-09-05 11:48:55 +0300

    feat: add accessibility version

--- src/public/js/components/header.js
+++ src/public/js/components/header.js
@@ -2,3 +2,29 @@ const loginButton = document.getElementById('loginButton');
 loginButton.addEventListener('click', () => {
     window.location.href = '../login.html';
 });
+
+const accessibilityToggle = document.getElementById('accessibilityToggle');
+const body   = document.body;
+
+document.addEventListener('DOMContentLoaded', () => {
+  const isOn = localStorage.getItem('accessible') === 'true';
+  setMode(isOn);
+});
+
+accessibilityToggle.addEventListener('click', () => {
+  const isOn = body.classList.toggle('accessible');
+  setMode(isOn);
+});
+
+function setMode(on) {
+    if (on) {
+        body.classList.add('accessible');
+        accessibilityToggle.setAttribute('aria-pressed', 'true');
+        accessibilityToggle.textContent = '👁';
+    } else {
+        body.classList.remove('accessible');
+        accessibilityToggle.setAttribute('aria-pressed', 'false');
+        accessibilityToggle.textContent = '👁';
+    } 
+    localStorage.setItem('accessible', on);
+}
